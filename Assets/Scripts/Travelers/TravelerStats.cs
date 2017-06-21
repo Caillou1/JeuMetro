5bc9eda883f2c846f011fd4e69b94d10422de41b
Pierre <pierrechapelet60@gmail.com>
2017-06-21 10:06:02 +0200

    Added Traveler stats

---
+++ Assets/Scripts/Travelers/TravelerStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TravelerStats : MonoBehaviour {
+	[Range(0f,100f)]
+	public float ElevatorAttraction;
+	[Range(0f,100f)]
+	public float EscalatorAttraction;
+	[Range(0f,100f)]
+	public float StairsAttraction;
+	[Range(0f,100f)]
+	public float RestPlaceAttraction;
+	[Range(0f,100f)]
+	public float Cleanliness;
+	[Range(0f,100f)]
+	public float VisualComprehension;
+	[Range(0f,100f)]
+	public float AudioComprehension;
+	[Range(0f,100f)]
+	public float TouchComprehension;
+	[Range(0f,100f)]
+	public float FaintnessPercentage;
+	public float MovementSpeed;
+}
