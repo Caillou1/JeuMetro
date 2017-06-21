551d30a8813815ea8e3a223e6b23599e58c38855
Pierre <pierrechapelet60@gmail.com>
2017-06-21 11:44:36 +0200

    Added WaveManager

--- Assets/Scripts/G.cs
+++ Assets/Scripts/G.cs
@@ -7,6 +7,7 @@ public sealed class G
     private static volatile G _instance;
     public Tilemap tilemap = new Tilemap();
     private GameManager _gameManager;
+	private WaveManager _waveManager;
 
     public static G Sys
     {
@@ -28,5 +29,13 @@ public sealed class G
             _gameManager = value;
         }
     }
-    
+
+	public WaveManager waveManager {
+		get { return _waveManager; }
+		set {
+			if (_waveManager != null)
+				Debug.Log ("2 waveManagers instanciated !");
+			_waveManager = value;
+		}
+	}
 }
---
+++ Assets/Scripts/Managers/WaveManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour {
+	public GameObject Traveler;
+	public Exit[] Exits;
+
+	private int ActualWave;
+	private Transform tf;
+
+	void Awake () {
+		G.Sys.waveManager = this;
+	}
+
+	void Start() {
+		tf = transform;
+
+		for (int i = 0; i < Exits.Length; i++) {
+			Exits [i].ActualWave = 0;
+			StartCoroutine (SpawnNextWave (i));
+		}
+	}
+
+	IEnumerator SpawnNextWave(int ExitNumber) {
+		var obj = Instantiate (Traveler, Exits [ExitNumber].exit.position, Quaternion.identity, tf);
+		var stats = obj.GetComponent<TravelerStats> ();
+		if (stats != null)
+			stats.SetStats (Exits [ExitNumber].Waves [Exits [ExitNumber].ActualWave].Stats);
+		else
+			Debug.Log ("TravelerStats Component Missing");
+
+		if (Exits [ExitNumber].ActualWave < Exits [ExitNumber].Waves.Length) {
+			yield return new WaitForSeconds (Exits [ExitNumber].Waves [Exits [ExitNumber].ActualWave].TimeBeforeNext);
+			Exits [ExitNumber].ActualWave++;
+			StartCoroutine (SpawnNextWave (ExitNumber));
+		}
+	}
+}
+
+[System.Serializable]
+public class Exit {
+	public Transform exit;
+	public Wave[] Waves;
+	public int ActualWave;
+}
+
+[System.Serializable]
+public class Wave {
+	public TravelerStats Stats;
+	public float TimeBeforeNext;
+}
