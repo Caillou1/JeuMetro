9717d05b5ce702aa2e27fc296eed32377f38868b
Pierre <pierrechapelet60@gmail.com>
2017-06-27 11:36:30 +0200

    Added Podotactile

---
+++ Assets/Scripts/DragAndDrop/DragAndDropPodotactile.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class DragAndDropPodotactile : DragAndDrop, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+
+	void IBeginDragHandler.OnBeginDrag(PointerEventData data) {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		Vector3 pos = ray.origin + (ray.direction * 1000);
+		InstantiatedObject = Instantiate (VirtualObjectToDrop, pos, Quaternion.identity).transform;
+	}
+
+	void IDragHandler.OnDrag(PointerEventData data) {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit)) {
+			if (hit.transform.CompareTag ("Ground")) {
+				Vector3 objPos = hit.transform.position;
+				InstantiatedObject.position = new Vector3 (Mathf.RoundToInt (objPos.x), Mathf.RoundToInt (objPos.y), Mathf.RoundToInt (objPos.z));
+			}
+		} else {
+			Vector3 pos = ray.origin + (ray.direction * 1000);
+			InstantiatedObject.position = new Vector3 (Mathf.RoundToInt (pos.x), Mathf.RoundToInt (pos.y), Mathf.RoundToInt (pos.z));
+		}
+	}
+
+	void IEndDragHandler.OnEndDrag(PointerEventData data) {
+		Orientation or = Orienter.angleToOrientation (InstantiatedObject.rotation.eulerAngles.y);
+
+		bool canPlace = true;
+		Vector3 dir = Orienter.orientationToDir3 (or);
+
+		//Case centrale
+		var v = G.Sys.tilemap.at (InstantiatedObject.position);
+		if (v.Count == 0 || v [0].type != TileID.GROUND || v[v.Count-1].type == TileID.PODOTACTILE)
+			canPlace = false;
+
+		PointerEventData pointerData = new PointerEventData(EventSystem.current);
+		List<RaycastResult> results = new List<RaycastResult>();
+
+		pointerData.position = Input.mousePosition;
+		EventSystem.current.RaycastAll(pointerData, results);
+
+		if ((results.Count > 0 && results [0].gameObject == gameObject) || !canPlace) {
+			Destroy (InstantiatedObject.gameObject);
+		} else {
+			Instantiate (ObjectToDrop, InstantiatedObject.position, InstantiatedObject.rotation);
+			Destroy (InstantiatedObject.gameObject);
+		}
+
+		InstantiatedObject = null;
+	}
+
+	void Update() {
+		if (Input.GetButtonDown ("Rotate")) {
+			RotateObject ();
+		}
+	}
+}
---
+++ Assets/Scripts/Tilemap/PodotactileTile.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class PodotactileTile : ATile
+{
+	private Transform tf;
+	private GameObject TwoBranchesStraight;
+	private GameObject TwoBranchesAngle;
+	private GameObject ThreeBranches;
+	private GameObject FourBranches;
+
+	void Awake()
+	{
+		tf = transform;
+
+		TwoBranchesStraight = tf.Find ("TwoStraight").gameObject;
+		TwoBranchesAngle = tf.Find ("TwoAngle").gameObject;
+		ThreeBranches = tf.Find ("Three").gameObject;
+		FourBranches = tf.Find ("Four").gameObject;
+
+		type = TileID.PODOTACTILE;
+
+		G.Sys.tilemap.addTile (tf.position, this, true, false, Tilemap.LOW_PRIORITY);
+
+		foreach (var t in G.Sys.tilemap.at(tf.position))
+			t.Connect ();
+
+		G.Sys.tilemap.addSpecialTile (type, tf.position);
+    }
+
+	public override void Connect ()
+	{
+		Vector3i pos = new Vector3i (transform.position);
+		List<ATile> list = new List<ATile> ();
+		List<Orientation> neighbors = new List<Orientation> ();
+
+		var l = G.Sys.tilemap.tilesOfTypeAt (pos + new Vector3i (Vector3.forward), TileID.PODOTACTILE);
+		if (l.Count > 0) {
+			Add (l [0], list);
+			neighbors.Add (Orientation.UP);
+		}
+
+		l = G.Sys.tilemap.tilesOfTypeAt (pos + new Vector3i (Vector3.back), TileID.PODOTACTILE);
+		if (l.Count > 0) {
+			Add (l [0], list);
+			neighbors.Add (Orientation.DOWN);
+		}
+
+		l = G.Sys.tilemap.tilesOfTypeAt (pos + new Vector3i (Vector3.right), TileID.PODOTACTILE);
+		if (l.Count > 0) {
+			Add (l [0], list);
+			neighbors.Add (Orientation.RIGHT);
+		}
+
+		l = G.Sys.tilemap.tilesOfTypeAt (pos + new Vector3i (Vector3.left), TileID.PODOTACTILE);
+		if (l.Count > 0) {
+			Add (l [0], list);
+			neighbors.Add (Orientation.LEFT);
+		}
+
+		switch (neighbors.Count) {
+		case 0:
+			TwoBranchesStraight.SetActive (true);
+			TwoBranchesAngle.SetActive (false);
+			ThreeBranches.SetActive (false);
+			FourBranches.SetActive (false);
+			break;
+
+		case 1:
+			tf.rotation = Quaternion.Euler(0, Orienter.orientationToAngle(neighbors[0]), 0);
+			TwoBranchesStraight.SetActive (true);
+			TwoBranchesAngle.SetActive (false);
+			ThreeBranches.SetActive (false);
+			FourBranches.SetActive (false);
+			break;
+
+		case 2:
+			if (Orienter.IsOppositeTo (neighbors [0], neighbors [1])) {
+				tf.rotation = Quaternion.Euler (0, Orienter.orientationToAngle (neighbors [0]), 0);
+				TwoBranchesStraight.SetActive (true);
+				TwoBranchesAngle.SetActive (false);
+				ThreeBranches.SetActive (false);
+				FourBranches.SetActive (false);
+			} else {
+				if (neighbors.Contains (Orientation.UP) && neighbors.Contains (Orientation.RIGHT)) {
+					tf.rotation = Quaternion.Euler (0, Orienter.orientationToAngle(Orientation.RIGHT), 0);
+				} else if (neighbors.Contains (Orientation.RIGHT) && neighbors.Contains (Orientation.DOWN)) {
+					tf.rotation = Quaternion.Euler (0, Orienter.orientationToAngle(Orientation.DOWN), 0);
+				} else if (neighbors.Contains (Orientation.DOWN) && neighbors.Contains (Orientation.LEFT)) {
+					tf.rotation = Quaternion.Euler (0, Orienter.orientationToAngle(Orientation.LEFT), 0);
+				} else if (neighbors.Contains (Orientation.LEFT) && neighbors.Contains (Orientation.UP)) {
+					tf.rotation = Quaternion.Euler (0, Orienter.orientationToAngle(Orientation.UP), 0);
+				}
+
+				TwoBranchesStraight.SetActive (false);
+				TwoBranchesAngle.SetActive (true);
+				ThreeBranches.SetActive (false);
+				FourBranches.SetActive (false);
+			}
+			break;
+
+		case 3:
+			if (!neighbors.Contains (Orientation.DOWN))
+				tf.rotation = Quaternion.Euler (0, Orienter.orientationToAngle (Orientation.DOWN), 0);
+			else if (!neighbors.Contains (Orientation.UP))
+				tf.rotation = Quaternion.Euler (0, Orienter.orientationToAngle (Orientation.UP), 0);
+			else if (!neighbors.Contains (Orientation.LEFT))
+				tf.rotation = Quaternion.Euler (0, Orienter.orientationToAngle (Orientation.LEFT), 0);
+			else if (!neighbors.Contains (Orientation.RIGHT))
+				tf.rotation = Quaternion.Euler (0, Orienter.orientationToAngle (Orientation.RIGHT), 0);
+			
+			TwoBranchesStraight.SetActive (false);
+			TwoBranchesAngle.SetActive (false);
+			ThreeBranches.SetActive (true);
+			FourBranches.SetActive (false);
+			break;
+
+		case 4:
+			TwoBranchesStraight.SetActive (false);
+			TwoBranchesAngle.SetActive (false);
+			ThreeBranches.SetActive (false);
+			FourBranches.SetActive (true);
+			break;
+		}
+
+		applyConnexions (list);
+	}
+
+	void OnDestroy()
+	{
+		G.Sys.tilemap.delTile (tf.position, this);
+		foreach (var t in G.Sys.tilemap.at(transform.position))
+			t.Connect ();
+		foreach (var t in connectedTiles)
+			t.targetOf.Remove (this);
+		foreach (var t in targetOf.ToList())
+			t.Connect ();
+	}
+}
