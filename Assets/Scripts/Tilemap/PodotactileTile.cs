32ff835c8fb1c36a76aaf222e80b1d83cfa44e9f
Pierre <pierrechapelet60@gmail.com>
2017-06-30 16:38:17 +0200

    fix podotactile

--- Assets/Scripts/Tilemap/PodotactileTile.cs
+++ Assets/Scripts/Tilemap/PodotactileTile.cs
@@ -50,10 +50,12 @@ public class PodotactileTile : ATile
 				if (!neighbors.Contains (Orientation.UP))
 					neighbors.Add (Orientation.UP);
 			} else if ((l [0].type == TileID.ESCALATOR) || (l[0].type == TileID.STAIRS )) {
-				if (!neighbors.Contains (Orientation.LEFT))
-					neighbors.Add (Orientation.LEFT);
-				if (!neighbors.Contains (Orientation.RIGHT))
-					neighbors.Add (Orientation.RIGHT);
+				if ((new Vector3i (l [0].transform.position)).equal (new Vector3i (tf.position + Vector3.forward * 2)) || (new Vector3i (l [0].transform.position)).equal (new Vector3i (tf.position + Vector3.forward + Vector3.down * 2))) {
+					if (!neighbors.Contains (Orientation.LEFT))
+						neighbors.Add (Orientation.LEFT);
+					if (!neighbors.Contains (Orientation.RIGHT))
+						neighbors.Add (Orientation.RIGHT);
+				}
 			} else {
 				if (!neighbors.Contains (Orientation.UP))
 					neighbors.Add (Orientation.UP);
@@ -73,10 +75,12 @@ public class PodotactileTile : ATile
 				if(!neighbors.Contains(Orientation.DOWN))
 					neighbors.Add (Orientation.DOWN);
 			} else if ((l[0].type == TileID.ESCALATOR) || (l[0].type == TileID.STAIRS)) {
-				if(!neighbors.Contains(Orientation.LEFT))
-					neighbors.Add (Orientation.LEFT);
-				if(!neighbors.Contains(Orientation.RIGHT))
-					neighbors.Add (Orientation.RIGHT);
+				if ((new Vector3i (l [0].transform.position)).equal (new Vector3i (tf.position + Vector3.back * 2)) || (new Vector3i (l [0].transform.position)).equal (new Vector3i (tf.position + Vector3.back + Vector3.down * 2))) {
+					if (!neighbors.Contains (Orientation.LEFT))
+						neighbors.Add (Orientation.LEFT);
+					if (!neighbors.Contains (Orientation.RIGHT))
+						neighbors.Add (Orientation.RIGHT);
+				}
 			} else {
 				if (!neighbors.Contains (Orientation.DOWN))
 					neighbors.Add (Orientation.DOWN);
@@ -96,10 +100,12 @@ public class PodotactileTile : ATile
 				if(!neighbors.Contains(Orientation.RIGHT))
 					neighbors.Add (Orientation.RIGHT);
 			} else if ((l[0].type == TileID.ESCALATOR) || (l[0].type == TileID.STAIRS)) {
-				if(!neighbors.Contains(Orientation.DOWN))
-					neighbors.Add (Orientation.DOWN);
-				if(!neighbors.Contains(Orientation.UP))
-					neighbors.Add (Orientation.UP);
+				if ((new Vector3i (l [0].transform.position)).equal (new Vector3i (tf.position + Vector3.right * 2)) || (new Vector3i (l [0].transform.position)).equal (new Vector3i (tf.position + Vector3.right + Vector3.down * 2))) {
+					if (!neighbors.Contains (Orientation.DOWN))
+						neighbors.Add (Orientation.DOWN);
+					if (!neighbors.Contains (Orientation.UP))
+						neighbors.Add (Orientation.UP);
+				}
 			} else {
 				if (!neighbors.Contains (Orientation.RIGHT))
 					neighbors.Add (Orientation.RIGHT);
@@ -119,10 +125,12 @@ public class PodotactileTile : ATile
 				if(!neighbors.Contains(Orientation.LEFT))
 					neighbors.Add (Orientation.LEFT);
 			} else if ((l[0].type == TileID.ESCALATOR) || (l[0].type == TileID.STAIRS)) {
-				if(!neighbors.Contains(Orientation.DOWN))
-					neighbors.Add (Orientation.DOWN);
-				if(!neighbors.Contains(Orientation.UP))
-					neighbors.Add (Orientation.UP);
+				if ((new Vector3i (l [0].transform.position)).equal (new Vector3i (tf.position + Vector3.left * 2)) || (new Vector3i (l [0].transform.position)).equal (new Vector3i (tf.position + Vector3.left + Vector3.down * 2))) {
+					if (!neighbors.Contains (Orientation.DOWN))
+						neighbors.Add (Orientation.DOWN);
+					if (!neighbors.Contains (Orientation.UP))
+						neighbors.Add (Orientation.UP);
+				}
 			} else {
 				if (!neighbors.Contains (Orientation.LEFT))
 					neighbors.Add (Orientation.LEFT);
