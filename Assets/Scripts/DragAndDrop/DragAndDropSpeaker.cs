150b785b4465e0bacf86d6ee8d2e502fb8597db4
Pierre <pierrechapelet60@gmail.com>
2017-06-30 10:23:22 +0200

    fix dad

--- Assets/Scripts/DragAndDrop/DragAndDropSpeaker.cs
+++ Assets/Scripts/DragAndDrop/DragAndDropSpeaker.cs
@@ -5,7 +5,7 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using DG.Tweening;
 
-public class DragAndDropInfoPanel : DragAndDrop {
+public class DragAndDropSpeaker : DragAndDrop {
 
 	private bool IsWalled;
 
