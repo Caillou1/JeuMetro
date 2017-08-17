21dbc315ec4ecc02ae5b6a929f23a43a3af59064
Pierre <pierrechapelet60@gmail.com>
2017-08-17 16:48:48 +0200

    push drag camera (PAS FINI DON'T PULL)

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class TutorialManager : MonoBehaviour {
 
-	// Use this for initialization
+
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+}
+
+public enum Objectif {
+	NONE
+}
+
+[System.Serializable]
+public class Tutorial {
+	public List<string> MessagesToShow;
+	public List<Objectif> Objectives;
+
+	public bool ObjectivesDone {
+		get{
+			foreach (var o in Objectives) {
+				if (!ObjectifChecker.Check (o)) {
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
+
+public static class ObjectifChecker {
+	public static bool Check(Objectif o) {
+		return true;
 	}
 }
